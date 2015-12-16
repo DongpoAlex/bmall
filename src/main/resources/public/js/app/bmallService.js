3a3d68bd27c8deb9064c2c4e75e788957c66a3a6
alexbob <billbob.dong@hotmail.com>
2015-12-17 03:22:25 +0800

    菜单更新完成

--- src/main/resources/public/js/app/bmallService.js
+++ src/main/resources/public/js/app/bmallService.js
@@ -1,8 +1,24 @@
 angular.module('bmallService', [])
     .factory('menu', ['$http', function ($http) {
-        return function(fnc){
-            $http.get('/api/dept').success(
-            fnc
-            );
+        return function (fnc) {
+            $http.get('/api/dept').success(fnc);
         }
-    }]);
+    }]).directive('handleSidebarMenu', function () {
+
+    function link(scope, element, attrs) {
+        var em =$(element);
+        em.click(function () {
+            if (em.hasClass("collapsed") == false) {
+                em.addClass("collapsed");
+                em.siblings(".dropdown-menu").slideDown(300);
+            } else {
+                em.removeClass("collapsed");
+                em.siblings(".dropdown-menu").slideUp(300);
+            }
+        });
+    };
+
+    return {
+        link: link
+    };
+});
