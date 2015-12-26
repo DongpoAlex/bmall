78d1def6ff7623bec858a8602ff0428446d45835
AlexBob <billbob@gmail.com>
2015-12-26 15:31:16 +0800

    增加网站进度条。。

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -22,14 +22,29 @@ angular.module('bMall', ['ngRoute', 'auth', 'home', 'navigation', 'bMallService'
     ]).run(function (auth) {
         auth.init('/', '/login', '/logout');
     }
-).controller("initCtrl", ['$rootScope', 'menuService', 'cartService',
+).directive('loading', ['$http', function ($http) {
+    return {
+        restrict: 'A',
+        link: function (scope, elm, attrs) {
+            scope.isLoading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+            scope.$watch(scope.isLoading, function (v) {
+                if (v) {
+                    elm.show();
+                } else {
+                    elm.hide();
+                }
+            });
+        }
+    };
+}]).controller("initCtrl", ['$rootScope', 'menuService', 'cartService',
     function ($rootScope, menuService, cartService) {
         $rootScope.removeGoods = cartService.remove;
         $rootScope.getTotal = cartService.getTotal;
         $rootScope.putPurchase = cartService.putPurchase;
 
-    }
-]).controller('goodsCtrl', ['$scope', '$http', '$routeParams', 'cartService', 'goodsService', 'filterFilter', '$rootScope',
+    }]).controller('goodsCtrl', ['$scope', '$http', '$routeParams', 'cartService', 'goodsService', 'filterFilter', '$rootScope',
     function ($scope, $http, $routeParams, cartService) {
         var params = $routeParams.id;
         $scope.goods = [];
