0aa1b3c0e5683e282a09f7caafd763aea16692be
AlexBob <billbob@gmail.com>
2015-12-29 13:37:37 +0800

    增加购物车样式。。

--- src/main/java/com/dongpo/bmall/config/WebSecurityConfiguration.java
+++ src/main/java/com/dongpo/bmall/config/WebSecurityConfiguration.java
@@ -41,7 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests()
-                .antMatchers("/*","/webjarslocator/**","/assets/**","/api/dept").permitAll()
+                .antMatchers("/*","/webjarslocator/**","/assets/**","/api/dept","/app/*").permitAll()
                 .anyRequest().authenticated().and().csrf()
                 .csrfTokenRepository(csrfTokenRepository()).and()
                 .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
