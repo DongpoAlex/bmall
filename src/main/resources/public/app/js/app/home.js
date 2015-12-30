ee43132cbbb548dca2fd9d80bdbd9db36484f902
AlexBob <billbob@gmail.com>
2015-12-31 02:25:45 +0800

    优化文件，取消多余辅助

--- src/main/resources/public/app/js/app/home.js
+++ src/main/resources/public/app/js/app/home.js
@@ -2,7 +2,7 @@ angular.module('home', ['bMallService']).controller('home', ['$rootScope', '$sco
     '$http', 'cartService',
     function ($rootScope, $scope,  goodsService, $http, cartService) {
 
-        goodsService.initGoodses('api/goods/search/byGuest?guestId=' + $rootScope.user.name + '&size=20');
+        goodsService.initGoodses('api/goods/search/byGuest?guestId=' + $rootScope.user.name + '&size=21');
 
         $scope.pageNext = goodsService.pageNext;
 
