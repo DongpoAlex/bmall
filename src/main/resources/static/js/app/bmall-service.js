9e4692a7f1b5dc1b27a60caf03c40a037130cd18
AlexBob <billbob@gmail.com>
2016-01-07 16:32:19 +0800

    调整主页面样式，取消多余的标签

--- src/main/resources/static/js/app/bmall-service.js
+++ src/main/resources/static/js/app/bmall-service.js
@@ -3,7 +3,7 @@ angular.module('bMallService', ['ngResource'])
         return $cacheFactory('super-cache');
     }])
     .factory('menuService', function ($rootScope, $resource, superCache) {
-        $rootScope.menus = [{name: "生鲜食材", prentId: 0, id: 3}];
+        $rootScope.menus = [];
         var initMenus = function () {
             if (superCache.get('menus')) {
                 $rootScope.menus = superCache.get("menus");
