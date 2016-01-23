86d004ac293a9a05bc2c5cd83a90ae399d19d377
AlexBob <billbob.dong@gmail.com>
2016-01-23 22:00:07 +0800

    优化项目文件，缩小发布文件大小

--- src/main/java/com/dongpo/bmall/Application.java
+++ src/main/java/com/dongpo/bmall/Application.java
@@ -2,19 +2,8 @@ package com.dongpo.bmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.HandlerMapping;
-import org.webjars.WebJarAssetLocator;
-
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 
 /**
  * Created by AlexBob on 2015/12/15.
@@ -25,20 +14,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @ResponseBody
-    @RequestMapping("/webjarslocator/{webjar}/**")
-    public ResponseEntity<? extends Object> locateWebjarAsset(@PathVariable String webjar, HttpServletRequest request) {
-        try {
-            String mvcPrefix = "/webjarslocator/" + webjar + "/"; // This prefix must match the mapping path!
-            String mvcPath = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-            String fullPath = new WebJarAssetLocator().getFullPath(webjar, mvcPath.substring(mvcPrefix.length()));
-            return new ResponseEntity<Object>(new ClassPathResource(fullPath), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
     /**
      * angular route
      * @return
